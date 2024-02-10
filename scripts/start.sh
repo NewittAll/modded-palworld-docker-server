65b2b1ee49a24afb0ce86596b85d5408ac04577c
NewittAll <newittr@gmail.com>
2024-02-09 23:11:45 -0700

    Clean up

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,6 +26,26 @@ function startServer() {
     echo ">>> Palworld server stopping"
 }
 
+function stop_server() {
+    echo -e "\x1b[31;1m>>> Stopping server \x1b[0m\n"
+    if [[ -n $RCON_ENABLED ]] && [[ $RCON_ENABLED == "true" ]]; then
+        save_and_shutdown_server
+    fi
+	kill -SIGTERM "$(pidof PalServer-Linux-Test)"
+	tail --pid="$(pidof PalServer-Linux-Test)" -f 2>/dev/null
+    if [[ -n $WEBHOOK_ENABLED ]] && [[ $WEBHOOK_ENABLED == "true" ]]; then
+        send_stop_notification
+    fi
+    ew ">>> Server stopped gracefully.\n\n"
+    exit 143;
+}
+
+function save_and_shutdown_server() {
+    rcon 'broadcast Server-shutdown-was-requested-init-saving'
+    rcon 'save'
+    rcon 'broadcast Done-saving-server-shuts-down-now'
+}
+
 function bootManager() {
     checkSettings
     setupCron
